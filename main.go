4888153f498ae551fd31a4b88e7d3c19fe408d37
Tim <030200531@hdjt.hdad.local>
2020-10-03 11:45:01 +0800

    gin-vue demo init with mysql and postman

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
+//User 定义model
+type User struct {
+	gorm.Model
+	Name      string `gorm:"type:varchar(20);not null"`
+	Telephone string `gorm:"type:varchar(110);not null;unique"`
+	Password  string `gorm:"size 255;not null"`
+}
+
 func main() {
+	db := InitDB()
+	defer db.Close()
 	r := gin.Default()
 	r.POST("/api/auth/register", func(ctx *gin.Context) {
 		//! 获取参数
@@ -35,8 +48,20 @@ func main() {
 		}
 		log.Println(name, telephone, password)
 		//! 判断手机号是否存在
+		if isTelephoneExist(db, telephone) {
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+				"code": 422,
+				"msg":  "用户已经存在",
+			})
+			return
+		}
 		//! 创建用户
-
+		newUser := User{
+			Name:      name,
+			Telephone: telephone,
+			Password:  password,
+		}
+		db.Create(&newUser)
 		//! 返回结果
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "注册成功",
@@ -54,4 +79,38 @@ func RandomString(n int) string {
 		result[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(result)
+}
+
+// InitDB 初始化数据库
+func InitDB() *gorm.DB {
+	driveName := "mysql"
+	host := "localhost"
+	port := "3306"
+	database := "ginvuedemo"
+	username := "user"
+	password := "user"
+	charset := "utf8"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+	db, err := gorm.Open(driveName, args)
+	if err != nil {
+		panic("failed to connect databse, err:" + err.Error())
+	}
+	//! 创建数据表
+	db.AutoMigrate(&User{})
+	return db
+}
+
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	return false
 }
